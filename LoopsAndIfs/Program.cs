27ca3cea9fe135aa6750c19feac7b376f97bedbb
Daniel Aranda Martin <danielarandamar88@gmail.com>
2022-10-16 19:10:30 -0500

    first commit

---
+++ LoopsAndIfs/Program.cs
@@ -0,0 +1,75 @@
+﻿// Logica booleana
+var cantidad1 = 2;
+var cantidad2 = 3;
+var resultado = cantidad1 == cantidad2;
+
+var nombre = "Daniel";
+var isNulo = nombre is null; // Buena practica en vez de utilizar ==
+var noEsNulo = nombre is not null;
+
+// Sentencia Swicth
+var year = 2022;
+switch(year)
+{
+    case 2021:
+        Console.WriteLine(2021);
+        break;
+    case 2022:
+        Console.WriteLine(2022);
+        break;
+}
+
+// Switch corto
+var lugar = 0;
+var message = lugar switch
+{
+    0 => "miami",
+    1 => "los angeles",
+    2 or 3 => "monterrey",
+    _ => "otro"
+};
+
+
+
+// Patrones relacionales
+var temp = 37;
+var mensaje = temp switch
+{
+    37 => "No hay condicional",
+    > 37 and < 39 => "Tienes temperatura",
+    >= 39 => "Temperatura muy alta",
+    < 37 => "Tienes una condicion"
+};
+Console.WriteLine(mensaje);
+
+var date = DateTime.Now;
+var estacion = date.Month switch
+{
+    1 or 2 or 3 => "Primavera",
+    4 or 5 or 6 => "Verano",
+    7 or 8 or 9 => "Otono",
+    10 or 11 or 12 => "Invierno",
+    _ => throw new ApplicationException("Mes no especificado")
+};
+
+Console.WriteLine("Aqui /////");
+var n = 15;
+for (int i = 1; i <= n; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+        continue;
+    }
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+        continue;
+    }
+    if (i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+        continue;
+    }
+    Console.WriteLine(i);
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Sentencias de alto nivel: (Top-Level Statements)
+// A partir de C# 9 fueron hechas las sentencias de alto nivel para evitar escribir
+// mucho codigo. En vez de escribir los namespace, la clase Program y demas codigo...
+// Detras de camara este codigo de sentencia de alto nivel es convertido en un
+// codigo especial que indica el punto de entrada de tu aplicacion.
+// (Solo puede haber un punto de entrada)
+//
+// Un programa solo puede tener un archivo con sentencias de alto nivel ya que
+// el programa solo admite un punto de entrada.
+
+Console.WriteLine("Hello, World!"); // Sentencia, esta termina con punto y coma
+
+int earthSunDistance = 150_000_000;
+Console.WriteLine(earthSunDistance);
+
+
+//  NUMEROS FLOTANTES
+//
+// Entre mayor espacio ocupa, mas precision tienen
+
+float altura = 175.5f; // De 6 a 9 digitos
+
+double anchura = 45.99; // De 15 a 17 digitos
+
+decimal tasaInteres = 9.99m; // De 28 a 29 digitos Recomendado para temas de dinero
+
+Console.WriteLine(altura);
+Console.WriteLine(anchura);
+Console.WriteLine(tasaInteres);
+
+string myCompleteName = "Daniel Aranda \" ";
+
+Console.WriteLine(myCompleteName);
+
+
+// STRINGS VERBATIM
+//
+// Nos permite tener varias lineas de texto y
+// ahorrarnos la secuencia de escape del backslash
+//
+string carta = @"A quien pueda interesar, 
+    Es timado cliente, deber mucho dinero.
+    Att: La administracion
+";
+
+string myPath = @"c:\www\wamp64\www";
+Console.WriteLine(myPath);
+
+
+// DateTime
+
+DateTime ahora = DateTime.Now;
+
+DateTime birthday = new DateTime(2022, 1, 10, 10, 00, 00);
+
+DateTime tomorrow = DateTime.Now.AddDays(1);
+
+
+// VALORES POR DEFECTO
+int edad = default; // Estoy asignando el valor por default para un int
+Console.WriteLine(edad);
+edad = 3;
+
+string myOtherName = null; // No hay referencia alguna a un valor en el heap
+
+
+// OPERACIONES UNARIAS
+var cantidad1 = 7;
+var cantidad2 = 5;
+
+// Primero se suma 1 a cantidad1 y luego se evalua toda la expresion
+var suma = ++cantidad1 + cantidad2; // 8 + 5
+// cantidad1 = cantidad1 + 1; (cantidad1 + cantdiad2); 
+Console.WriteLine(suma);
+
+// Primero se evalua toda la expresion y luego se le suma 1 a cantidad1
+var suma2 = cantidad1++ + cantidad2; // 7 + 5
+// (cantidad1 + cantdiad2); cantidad1 = cantidad1 + 1;
+Console.WriteLine(suma2);
+
+// CONSTANTES
+
+const int miEdad = 21;
